ead0842510daa96326bf7c1c82dcc292d574f836
wahid9000 <wahidchowdhury9000@gmail.com>
2023-03-28 15:52:00 +0600

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <nav className='header'>
+        <nav className = 'header'>
             <img src={logo} alt="" />
             <div>
                 <a href="/shop">Shop</a>
