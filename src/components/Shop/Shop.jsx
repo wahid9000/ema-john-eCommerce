dcb7c6bd03041d0c0bd74d387d515015beb8e035
wahid9000 <wahidchowdhury9000@gmail.com>
2023-03-29 16:48:23 +0600

    show data in cart with proper calculation

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -36,7 +36,22 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        let newCart = [];
+
+        //if product doesnt exist in the cart, set quantity = 1
+        //if exists update quantity by 1
+        
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id);
     }
