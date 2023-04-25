17465c9a227bac225e2743bb34ee7421c72b2675
wahid9000 <wahidchowdhury9000@gmail.com>
2023-04-25 14:40:21 +0600

    added login and registration page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+
+
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label><br />
+                    <input type="email" name="email" id=""  required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label><br />
+                    <input type="password" name="password" id=""  required />
+                </div>
+                <div className='form-control'>
+                    <button className='login-btn'>Login</button>
+                </div>
+                <div>
+                    
+                </div>
+            </form>
+         
+            <p className='new-account-btn'>Don't have an Account? <span><Link className='register-btn' to="/register">Create Now!</Link> </span></p>
+            <p className='or'>or</p>
+
+            <div className='form-control'>
+                <button className='google-btn'>Continue With Google</button>
+            </div>
+
+
+      
+            
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import './Register.css'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is registration page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label><br />
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label><br />
+                    <input type="password" name="password" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirmPassword">Confirm Password</label><br />
+                    <input type="confirmPassword" name="confirmPassword" id="" required />
+                </div>
+                <div className='form-control'>
+                    <button className='login-btn'>Sign Up</button>
+                </div>
+                <div>
+
+                </div>
+            </form>
+
+            <p className='new-account-btn'>Already have an Account? <span><Link className='register-btn' to="/login">Login Here!</Link> </span></p>
+            <p className='or'>or</p>
+
+            <div className='form-control'>
+                <button className='google-btn'>Continue With Google</button>
+            </div>
+
+
+
         </div>
     );
 };
